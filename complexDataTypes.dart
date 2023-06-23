6240ff64c617179195bdb4d53e35ab7ff739b8ca
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-06-22 23:26:56 -0700

    video 13 - data types

---
+++ complexDataTypes.dart
@@ -0,0 +1,18 @@
+
+//complex data
+void main(){
+
+String stringOutput = 'Lorem Ipsum ';
+print(stringOutput);
+print('HI There!');
+print('$stringOutput');
+
+
+//keywords
+late int exampleInt;
+exampleInt = 34;
+print(exampleInt);
+
+final int finalInt = 3;
+print(finalInt);
+}
---
+++ primitiveDataTypes.dart
@@ -0,0 +1,11 @@
+
+//primitive data
+void main(){
+  bool boolOutput = false;
+  print(boolOutput);
+  int intOutput = 23;
+  print(intOutput);
+  double doubleOutput = 23.42;
+  print(doubleOutput);
+
+}
